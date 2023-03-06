1a79053d950d732cca0139366c057398e721e94a
raquelindia <raquelicruz94@gmail.com>
2023-03-06 15:21:27 -0500

    add express route

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ const {sequelize} = require("./db");
 const port = 3000;
 
 //TODO: Create your GET Request Route Below: 
+app.get("/restaurants", async (request, response) => {
+        const getRestaurants = await Restaurant.findAll();
+        response.json(getRestaurants);
 
+})
 
 app.listen(port, () => {
     sequelize.sync();
-    console.log("Your server is listening on port " + port);
+    console.log(`Your server is listening on port http://localhost:${port}/restaurants`);
 })
