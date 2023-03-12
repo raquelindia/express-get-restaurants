81321e9b0c295264afc961f2e1c0cb54167c4bee
raquelindia <raquelicruz94@gmail.com>
2023-03-12 17:43:13 -0400

    added express router

---
+++ routes/restaurants.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const router = express.Router();
+const app = express();
+const {Restaurant} = require("../models/index");
+const {sequelize} = require("../db");
+//const {check, validationResult} = require("express-validator");
+app.use(express.json());
+
+
+
+
+
+router.get("/", async (request, response) => {
+    const getRestaurants = await Restaurant.findAll();
+    response.json(getRestaurants);
+
+})
+
+router.get("/:id", async (request, response) => {
+const restaurant = await Restaurant.findByPk(request.params.id);
+response.json(restaurant);
+})
+
+
+router.post("/", async (req, res) => {
+const newName = req.body.name;
+const newLocation = req.body.location;
+const newCuisine = req.body.cuisine;
+
+const newRestaurant = await Restaurant.create({name: newName, location: newLocation, cuisine: newCuisine});
+res.json(newRestaurant);
+})
+
+router.put("/:id", async (req, res) => {
+
+const id = req.params.id
+const updatedName = req.body.name;
+const updatedLocation = req.body.location;
+const updatedCuisine = req.body.cuisine;
+const foundRestaurant = await Restaurant.findByPk(id);
+
+const updatedRestaurant = await foundRestaurant.update({
+    name: updatedName,location: updatedLocation, cuisine: updatedCuisine});
+
+res.json(updatedRestaurant);
+
+});
+
+
+router.delete("/:id", async (req, res) => {
+const id = req.params.id
+const restaurantRow = await Restaurant.findByPk(id);
+const deletedRestaurants = await restaurantRow.destroy()
+const foundRestaurant = await Restaurant.findByPk(id);
+res.json(deletedRestaurants);
+
+})
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,59 +1,18 @@
 const express = require("express");
 const app = express();
+const router = express.Router();
 const {Restaurant} = require("./models/index")
 const {sequelize} = require("./db");
 
-const port = 3001;
+const port = 3002;
 app.use(express.json())
 
 sequelize.sync();
 
-
-app.get("/restaurants", async (request, response) => {
-        const getRestaurants = await Restaurant.findAll();
-        response.json(getRestaurants);
-
-})
-
-app.get("/restaurants/:id", async (request, response) => {
-    const restaurant = await Restaurant.findByPk(request.params.id);
-    response.json(restaurant);
-})
+const restaurantRouter = require("./routes/restaurants");
+app.use("/restaurants", restaurantRouter);
 
 
-app.post("/restaurants", async (req, res) => {
-    const newName = req.body.name;
-    const newLocation = req.body.location;
-    const newCuisine = req.body.cuisine;
-
-    const newRestaurant = await Restaurant.create({name: newName, location: newLocation, cuisine: newCuisine});
-    res.json(newRestaurant);
-})
-
-app.put("/restaurants/:id", async (req, res) => {
-
-    const id = req.params.id
-    const updatedName = req.body.name;
-    const updatedLocation = req.body.location;
-    const updatedCuisine = req.body.cuisine;
-    const foundRestaurant = await Restaurant.findByPk(id);
-
-    const updatedRestaurant = await foundRestaurant.update({
-        name: updatedName,location: updatedLocation, cuisine: updatedCuisine});
-
-    res.json(updatedRestaurant);
-   
-});
-
-
-app.delete("/restaurants/:id", async (req, res) => {
-    const id = req.params.id
-    const restaurantRow = await Restaurant.findByPk(id);
-    const deletedRestaurants = await restaurantRow.destroy()
-    const foundRestaurant = await Restaurant.findByPk(id);
-    res.json(deletedRestaurants);
-
-})
 
 
 app.listen(port, () => {
